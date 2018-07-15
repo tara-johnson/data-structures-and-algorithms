7a60087cbac2f82fb3296ed7ab35e1a1e0aacfa7
Tara Johnson <tara@madface.com>
2018-07-14 23:26:20 -0700

    Unable to switch branches after OS switch

--- src/day02/ArrayShift.java
+++ src/day02/ArrayShift.java
@@ -1,34 +1,24 @@
 package day02;
 
-import java.util.Arrays;
-
 public class ArrayShift {
     public static void main(String[] args) {
-        int[] firstArr = {2, 4, 6, 8};
+        int[] arr = {2, 4, 6, 8};
         int newVal = 5;
-        insertShiftArray(firstArr, newVal);
+        insertShiftArray(arr, newVal);
     }
 
-    public static void insertShiftArray(int[] firstArr, int newVal) {
-        int length = firstArr.length;
-//        int[] secondArr = new int[length + 1];
+    public static int[] insertShiftArray(int[] arr, int newVal) {
+        int[] secondArr = new int[arr.length + 1];
+        int midIndex = arr.length / 2;
 
-        int[] tempA = new int[(length + 1) / 2];
-        int[] tempB = new int[length - tempA.length];
-
-        for (int i = 0; i < length; i++) {
-            if (i < tempA.length) {
-                tempA[i] = firstArr[i];
-            } else {
-                tempB[i - tempA.length] = firstArr [i];
-            }
+        for (int i = 0; i <= midIndex; i++) {
+            secondArr[i] = arr[i];
+        }
+        secondArr[(arr.length + 1) / 2] = newVal;
+        for (int i = midIndex + 1; i < arr.length; i++) {
+            secondArr[i+1] = arr[i];
         }
 
-//        int[] secondArr = new int[tempA + tempB];
-//        System.out.print(Arrays.toString(secondArr));
-
-        System.out.println(Arrays.toString(tempA));
-        System.out.println(newVal);
-        System.out.println(Arrays.toString(tempB));
+        return secondArr;
     }
 }
--- src/day02/ArrayShiftTest.java
+++ src/day02/ArrayShiftTest.java
@@ -2,17 +2,39 @@ package day02;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ArrayShiftTest {
 
     @Test
-    void insertShiftArray() {
-        int[] numbers = {2, 4, 6, 8};
-        int n = 5;
-        ArrayShift.insertShiftArray(numbers, n);
+    void singleDigitArrayTest() {
+        int[] arr = {1};
+        int newVal = 2;
+        String result = Arrays.toString(ArrayShift.insertShiftArray(arr, newVal));
 
-        int[] expected = {2, 4, 5, 6, 8};
-        assertArrayEquals(expected, numbers);
+        String expected = Arrays.toString(new int[] {1, 2});
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void oddArrayTest() {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
+        int newVal = 10;
+        String result = Arrays.toString(ArrayShift.insertShiftArray(arr, newVal));
+
+        String expected = Arrays.toString(new int[] {1, 2, 3, 4, 10, 5, 6, 7});
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void evenArrayTest() {
+        int[] arr = {2, 4, 6, 8};
+        int newVal = 5;
+        String result = Arrays.toString(ArrayShift.insertShiftArray(arr, newVal));
+
+        String expected = Arrays.toString(new int[] {2, 4, 5, 6, 8});
+        assertEquals(expected, result);
     }
 }
