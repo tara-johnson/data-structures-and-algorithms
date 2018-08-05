0893b5971241dafd625aabc66530ac7d9678865f
Tara Johnson <tara@madface.com>
2018-08-04 23:19:30 -0700

    Put methods in Binary Tree class. Empty tree test passing.

---
+++ src/day19/BinaryTree.java
@@ -0,0 +1,171 @@
+package day19;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BinaryTree {
+    public TreeNode root;
+
+    // Returns the root node
+    public TreeNode getRoot() {
+        return root;
+    }
+
+    // Adds a node with a specific value to the tree
+    public void add(int val) {
+        if (this.root == null) {
+            this.root = new TreeNode(val);
+            return;
+        }
+        this.add(val, this.root);
+    }
+
+    // Move through the tree until we find a free
+    // spot to place the new value
+    private void add(int val, TreeNode current) {
+        // found a proper spot on the left!
+        if (current.left == null && val <= current.data) {
+            current.left = new TreeNode(val);
+            return;
+        }
+
+        // Found a proper spot on the right!
+        if (current.right == null && val > current.data) {
+            current.right = new TreeNode(val);
+            return;
+        }
+
+        // Recursive case: move through the tree
+        // Move left and right through the tree comparing values
+        if (val <= current.data)  {
+            add(val, current.left);
+        } else if (val > current.data) {
+            add(val, current.right);
+        }
+    }
+
+    // Public traversal method that visits current, left, and right
+    private StringBuilder preOrder(StringBuilder builder, TreeNode current) {
+        if (current == null) {
+            return builder;
+        }
+
+        builder.append(current.data + " ");
+        preOrder(builder, current.left);
+        preOrder(builder, current.right);
+
+        return builder;
+    }
+
+    // Public traversal method that visits left, current, and right
+    public StringBuilder inOrder(StringBuilder builder, TreeNode current) {
+        if (current == null) {
+            return builder;
+        }
+
+        inOrder(builder, current.left);
+        builder.append(current.data + " ");
+        inOrder(builder, current.right);
+
+        return builder;
+    }
+
+    // Public traversal method that visits left, right, and current
+    public StringBuilder postOrder(StringBuilder builder, TreeNode current) {
+        if (current == null) {
+            return builder;
+        }
+
+        postOrder(builder, current.left);
+        preOrder(builder, current.right);
+        builder.append(current.data + " ");
+
+        return builder;
+    }
+
+    // Public method that returns an in-order traversal of the tree as a String
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        return inOrder(builder, this.root).toString();
+    }
+
+    // Public method that returns the number of nodes in the tree
+    public int size() {
+        return (size(root));
+    }
+
+    private int size(TreeNode current) {
+        if (current == null) {
+            return 0;
+        } else {
+            return(size(current.left) + size(current.right) + 1);
+        }
+    }
+
+    // Public method that returns the minimum value in the tree
+    public int min() {
+        // Sets a pointer to the root node then always moves left
+        TreeNode current = this.root;
+        while (current.left != null) {
+            current = current.left;
+        }
+        return current.data;
+    }
+
+    // Public method that returns the maximum value in the tree
+    public int max() {
+        // Sets a pointer to the root node then always moves right
+        TreeNode current = this.root;
+        while (current.right != null) {
+            current = current.right;
+        }
+        return current.data;
+    }
+
+    // Public method that returns true or false if the given value is anywhere in the tree
+    public boolean contains(int val) {
+        return this.contains(val, this.root);
+    }
+
+    private boolean contains(int val, TreeNode node) {
+        if (node == null) {
+            return false;
+        }
+
+        if (node.data == val) {
+            return true;
+        }
+
+        boolean wasOnLeft = contains(val, node.left);
+        boolean wasOnRight = contains(val, node.right);
+        return wasOnLeft || wasOnRight;
+    }
+
+    public boolean isIdentical(BinaryTree input) {
+        return compareTrees(root, input.root);
+    }
+
+    public static boolean compareTrees(TreeNode root1, TreeNode root2) {
+        // Create queues to hold the tree nodes
+        Queue<TreeNode> qq1 = new LinkedList<>();
+        Queue<TreeNode> qq2 = new LinkedList<>();
+
+        // Add respective tree roots to the queues
+        qq1.add(root1);
+        qq2.add(root2);
+
+        System.out.println("qq1: " + qq1);
+        System.out.println("qq2: " + qq2);
+
+        // Declare isIdentical variable, instantiated as true
+        boolean isIdentical = true;
+
+        // If both queues are empty return true (they match)
+        if (qq1 == null && qq2 == null) {
+            System.out.println("The tree is empty");
+            return isIdentical;
+        }
+
+        return isIdentical;
+    }
+}
--- src/day19/IsIdenticalTest.java
+++ src/day19/IsIdenticalTest.java
@@ -1,7 +1,97 @@
 package day19;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class IsIdenticalTest {
 
+    @Test
+    void isIdenticalEmptyTreeTest() {
+        // Construct first tree
+        BinaryTree tree1 = new BinaryTree();
+
+        // Construct second tree
+        BinaryTree tree2 = new BinaryTree();
+
+        Boolean expected = true;
+        Boolean actual = tree1.isIdentical(tree2);
+
+        assertEquals(expected, actual);
+    }
+
+//    @Test
+//    void isIdenticalTreeTest() {
+//        // Construct first tree
+//        BinaryTree tree1 = new BinaryTree();
+//        TreeNode n1 = new TreeNode(2);
+//
+//        TreeNode n2 = new TreeNode(7);
+//        TreeNode n3 = new TreeNode(2);
+//        TreeNode n4 = new TreeNode(6);
+//        TreeNode n5 = new TreeNode(5);
+//        TreeNode n6 = new TreeNode(11);
+//
+//        TreeNode n7 = new TreeNode(5);
+//        TreeNode n8 = new TreeNode(9);
+//        TreeNode n9 = new TreeNode(18);
+//
+//        // Connect nodes
+//        tree1.root = n1;
+//        System.out.println("Tree 1 root: " + tree1.root);
+//
+//        n1.left = n2;
+//        n2.left = n3;
+//        n2.right = n4;
+//        n4.left = n5;
+//        n4.right = n6;
+//
+//        n1.right = n7;
+//        n7.right = n8;
+//        n8.left = n9;
+//
+//        // Construct second tree
+//        BinaryTree tree2 = new BinaryTree();
+//        TreeNode nn1 = new TreeNode(3);
+//
+//        TreeNode nn2 = new TreeNode(7);
+//        TreeNode nn3 = new TreeNode(2);
+//        TreeNode nn4 = new TreeNode(6);
+//        TreeNode nn5 = new TreeNode(5);
+//        TreeNode nn6 = new TreeNode(11);
+//
+//        TreeNode nn7 = new TreeNode(5);
+//        TreeNode nn8 = new TreeNode(9);
+//        TreeNode nn9 = new TreeNode(18);
+//
+//        // Connect nodes
+//        tree2.root = nn1;
+//        System.out.println("Tree 2 root: " + tree2.root);
+//
+//        nn1.left = nn2;
+//        nn2.left = nn3;
+//        nn2.right = nn4;
+//        nn4.left = nn5;
+//        nn4.right = nn6;
+//
+//        nn1.right = nn7;
+//        nn7.right = nn8;
+//        nn8.left = nn9;
+//
+//        BinaryTree.compareTrees(tree1.root, tree2.root);
+//
+//        tree1.isIdentical(tree2);
+
+//        Boolean expected = tree1.isIdentical(tree2);
+//        System.out.println("Expected: " + expected);
+
+
+//        Boolean result = true;
+//        Boolean expected = tree1.isIdentical(tree2.root);
+//
+//        System.out.println("Result: " + result);
+//        System.out.println("Expected: " + expected);
+//
+//        assertEquals(result, expected);
+//    }
 }
