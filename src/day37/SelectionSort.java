5955b06a6b3623e093ce4bb341d582513f0f2447
Tara Johnson <tara@madface.com>
2018-08-29 15:50:31 -0700

    SelectionSort code and tests.

---
+++ src/day37/SelectionSort.java
@@ -0,0 +1,29 @@
+package day37;
+
+public class SelectionSort {
+    // References:
+    // https://www.geeksforgeeks.org/selection-sort/
+    // https://stackoverflow.com/questions/34023517/selection-sort-for-doubles-in-java/34023603
+
+    public static double[] selectionSort (double[] inputArray) {
+
+        // Iterate through the array one by one
+        for (int i = 0; i < inputArray.length - 1; i++) {
+
+            // Find the minimum value in the unsorted array
+            int min = i;
+            for (int j = i + 1; j < inputArray.length; j++) {
+                if (inputArray[j] < inputArray[min]) { ;
+                    min = j;
+                }
+            }
+
+            // Create a temporary variable to store the minimum value
+            // Swap the minimum value with the value at the index where the minimum value should be
+            double temp = inputArray[min];
+            inputArray[min] = inputArray[i];
+            inputArray[i] = temp;
+        }
+        return inputArray;
+    }
+}
---
+++ src/day37/SelectionSortTest.java
@@ -0,0 +1,60 @@
+package day37;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SelectionSortTest {
+    @Test
+    void emptyArrayInputSelectionSortTest() {
+        double[] inputArr = {};
+
+        double[] actual = SelectionSort.selectionSort(inputArr);
+        double[] expected = {};
+
+        System.out.println(Arrays.toString(actual));
+        System.out.println(Arrays.toString(expected));
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void singleArrayInputSelectionSortTest() {
+        double[] inputArr = {23.45};
+
+        double[] actual = SelectionSort.selectionSort(inputArr);
+        double[] expected = {23.45};
+
+        System.out.println(Arrays.toString(actual));
+        System.out.println(Arrays.toString(expected));
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void selectionSortTest() {
+        double[] inputArr = {8.4, 5.2 ,3 ,4 ,2.7 ,7 ,1 ,6};
+
+        double[] actual = SelectionSort.selectionSort(inputArr);
+        System.out.println(Arrays.toString(actual));
+
+        double[] expected = {1, 2.7, 3, 4, 5.2, 6, 7, 8.4};
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void selectionSortNegativeNumberTest() {
+        double[] inputArr = {34.2, 19, 42.7, -9.4, 2018, 0.5, 2005, 77, 2099.2};
+
+        double[] actual = SelectionSort.selectionSort(inputArr);
+        double[] expected = {-9.4, 0.5, 19, 34.2, 42.7, 77, 2005, 2018, 2099.2};
+
+        System.out.println(Arrays.toString(actual));
+        System.out.println(Arrays.toString(expected));
+
+        assertArrayEquals(expected, actual);
+    }
+}
